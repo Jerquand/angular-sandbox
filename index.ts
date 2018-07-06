88742a77660ab69b3afd12b62941622cdfc54f84
Jerquan Dickson <jerquand@yahoo.com>
2018-07-06 08:42:19 -0400

    Left off

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 const somethingFun: string = 'Riding my bike is fun!';
 console.log(somethingFun);
+const sandwich: string = 'BLT'; // string
+const orderNumber: number = 1738; // number
+const delicious: boolean = true; // boolean
