a27a03ad6fc1a11f6b7b8720d218159c600c5903
Jerquan Dickson <jerquand@yahoo.com>
2018-07-06 11:12:52 -0400

    github

--- index.ts
+++ index.ts
@@ -1,5 +1,32 @@
-const somethingFun: string = 'Riding my bike is fun!';
-console.log(somethingFun);
-const sandwich: string = 'BLT'; // string
-const orderNumber: number = 1738; // number
-const delicious: boolean = true; // boolean
+interface Person {
+    firstName: string;
+    lastName: string;
+    birthday: Date;
+}
+
+interface Contact {
+    phoneNumber: number;
+    email: string;
+    slack: string;
+    formatNumber: void;
+}
+
+class ContactCard implements Person, Contact {
+    firstName: string;
+    phoneNumber: number;
+    constructor(firstName: string) {
+        this.firstName= new Person();
+    }
+
+    sendMessage(){
+        console.log(this.sendMessage!());
+    }
+}
+   addToFavortes(){
+        console.log(this.sendMessage!());
+    }
+}
+
+const ContactCard = new Person(sam);
+
+ContactCard.printName();
